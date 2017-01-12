a7ca52600e54263f46a77563abee612411b29935
Huan Li <li.huan@zhtu.net>
2017-01-12 19:48:55 +0800

    commit 2017-1-6

--- client/imports/entry.ts
+++ client/imports/entry.ts
@@ -1,16 +1,59 @@
 'use strict';
+'use strict';
 
+import {Component, NgZone} from '@angular/core';
+import {MeteorComponent} from "angular2-meteor";
+import {Tracker} from "meteor/tracker";
+//noinspection TypeScriptCheckImport
+import template from './hello.html';
+import {NavController} from 'ionic-angular';
+import { Meteor } from 'meteor/meteor'
+import {Accounts} from "meteor/accounts-base";
 import {Component, OnInit} from "@angular/core";
 import { AccountsModule } from 'angular2-meteor-accounts-ui';
 import {HelloComponent} from './views/hello/hello';
-
+import {R3TToastService} from './services/service-test';
 @Component({
     template: '<ion-nav [root]="root"></ion-nav>'
 })
 
-export class EntryComponent implements OnInit {
+export class EntryComponent extends MeteorComponent{
     root = HelloComponent;
 
-    ngOnInit(): void {
+    // ngOnInit(): void {
+    // }
+    constructor(private zone: NgZone,
+                private toastService: R3TToastService) {
+        super();
+
+        this.autorun(() => {
+            // if(!Accounts.validateLoginAttempt)
+            // {
+            //     console.log("登录过期if");
+            // }
+            // let _logout = Meteor.logout;
+            // if(_logout){
+            //     console.log("登录过期if");
+            //     this.toastService.show('登录已过期');
+            // }Meteor.loggingIn()
+
+            Accounts.onLogin(function () {
+                Meteor.logoutOtherClients(function (){
+                    console.log("登录过期autorun");
+                });
+
+             });
+             // Accounts.onlogout(function () {
+             //     console.log('登出');
+             // });
+            // Accounts.loggingIn()
+            this.zone.run(() => {
+
+                //this.toastService.show('登录已过期');
+                console.log("登录过期zone");
+
+            });
+        });
+
     }
 }
--- client/imports/views/hello/hello.ts
+++ client/imports/views/hello/hello.ts
@@ -4,10 +4,14 @@ import {Component, NgZone} from '@angular/core';
 import {MeteorComponent} from "angular2-meteor";
 import {Tracker} from "meteor/tracker";
 //noinspection TypeScriptCheckImport
+
 import template from './hello.html';
+//noinspection TypeScriptCheckImport
+import { AccountsCommon } from 'meteor/accounts-base'
 import {NavController} from 'ionic-angular';
 import { Meteor } from 'meteor/meteor'
 import {Accounts} from "meteor/accounts-base";
+
 import {R3TToastService} from "../../services/service-test";
 interface Item {
     title: string;
@@ -50,27 +54,40 @@ export class HelloComponent extends MeteorComponent {
             },
         ];
 
-        this.autorun(() => {
-
-            this.zone.run(() => {
-                Accounts.onLogin(function () {
-                    Meteor.logoutOtherClients(function (){
-                        console.log("登录过期");
-                        //this.toastService.show('登录已过期');
-                    });
-                });
-                //this.toastService.show('登录已过期');
-
-            });
-        });
-
+        // this.autorun(() => {
+        //
+        //     this.zone.run(() => {
+        //         // if(!Meteor.user())
+        //         //     console.log("登录过期if");
+        //         Accounts.onLogin(function () {
+        //             Meteor.logoutOtherClients(function (){
+        //                 console.log("登录过期");
+        //                 //this.toastService.show('登录已过期');
+        //             });
+        //         });
+        //         //this.toastService.show('登录已过期');
+        //
+        //     });
+        // });
     }
 
     logout() {
-        this.toastService.show('登录已过期');
-        Accounts.onLogin(function () {
-            Meteor.logoutOtherClients();
+        AccountsCommon.onLogout(function () {
         });
-        //Meteor.logoutOtherClients();
+        // Accounts.logout(function () {
+        //     console.log('登出');
+        // });
+        //
+        // this.toastService.show('登录已过期');
+        // Accounts.onLogin(function () {
+        //     Meteor.logoutOtherClients();
+        // });
+        //  Meteor.logoutOtherClients();
+         Accounts.logoutOtherClients(function () {
+             console.log('-------------');
+             console.log(Accounts.logoutOtherClients());
+             console.log('-------------');
+         });
+
     }
 }
